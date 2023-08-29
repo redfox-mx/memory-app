1635c773e5482fdc1d4f2d91194385a12445f8c5
redfox-mx <hernandezdj98@outlook.com>
2023-08-29 05:39:11 -0600

    feat(icons): setup material icons registry

---
+++ src/app/components/icons/index.ts
@@ -0,0 +1,26 @@
+import { Injector, NgModule } from '@angular/core';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+
+/**
+ * Allow setup mat icons module
+ * @param registry icon registry form icons services
+ */
+export function bootstrapIcons(registry: MatIconRegistry) {
+  registry.setDefaultFontSetClass('material-symbols-rounded')
+}
+
+
+/**
+ * Facade over ´MatIconModule´ and custom icon typography
+ */
+@NgModule({
+  imports: [MatIconModule],
+  exports: [MatIconModule]
+})
+export class IconsModule {
+
+  constructor(injector: Injector) {
+    const matIconRegistry = injector.get(MatIconRegistry)
+    bootstrapIcons(matIconRegistry);
+  }
+}
