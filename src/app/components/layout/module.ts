4dba345d89c532d7cbe4f922b194db1d5b60317d
redfox-mx <hernandezdj98@outlook.com>
2023-08-30 18:00:15 -0600

    fix(board): set board size for diferent devices
    initial game load calculate board size based on device resolution

--- src/app/components/layout/module.ts
+++ src/app/components/layout/module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core'
-import { HeaderComponent } from './header/header'
-import { CommonModule } from '@angular/common'
-import { IconsModule } from '../icons'
+import { NgModule } from '@angular/core';
+import { LayoutModule as CdkLayoutModule } from '@angular/cdk/layout';
+import { HeaderComponent } from './header/header';
+import { CommonModule } from '@angular/common';
+import { IconsModule } from '../icons';
 
 
 @NgModule({
-  declarations: [ HeaderComponent ],
-  imports: [ CommonModule, IconsModule ],
-  exports: [ HeaderComponent ]
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    IconsModule,
+    CdkLayoutModule,
+  ],
+  exports: [
+    CdkLayoutModule,
+    HeaderComponent
+  ]
 })
 export class LayoutModule {}
